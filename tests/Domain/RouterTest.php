61f89e7126f078e7d879eb1f77822c6722426e43
Karol Mocniak <karol@mocniak.pl>
2019-05-16 23:51:12 +0200

    some comments

--- tests/Domain/RouterTest.php
+++ tests/Domain/RouterTest.php
@@ -49,23 +49,23 @@ class RouterTest extends TestCase
     public function testRouterHandlesConnectingARouteAroundTheWorld()
     {
         $cards = [
-            new BoardingCard('Berlin', 'Dusseldorf', 'train', '123A', ''),
-            new BoardingCard('Dusseldorf', 'Amsterdam', 'train', '123A', ''),
+            new BoardingCard('Berlin', 'Dusseldorf', 'bus', null, ''),
+            new BoardingCard('Dusseldorf', 'Amsterdam', 'train', '432', ''),
             new BoardingCard('Warsaw', 'Berlin', 'train', '16b', ''),
             new BoardingCard('Amsterdam', 'Brussels', 'train', '123A', ''),
-            new BoardingCard('Brussels', 'Paris', 'train', '123A', ''),
-            new BoardingCard('London', 'New York', 'train', '123A', ''),
-            new BoardingCard('Chicago', 'Los Angeles', 'train', '123A', ''),
-            new BoardingCard('Paris', 'London', 'train', '123A', ''),
-            new BoardingCard('Los Angeles', 'Honolulu', 'train', '123A', ''),
-            new BoardingCard('New York', 'Chicago', 'train', '123A', ''),
-            new BoardingCard('Honolulu', 'Tokio', 'train', '123A', ''),
+            new BoardingCard('Brussels', 'Paris', 'train', '17F', ''),
+            new BoardingCard('London', 'New York', 'plane', '13A', 'Cabin baggage only'),
+            new BoardingCard('Chicago', 'Los Angeles', 'rental car', '123A', ''),
+            new BoardingCard('Paris', 'London', 'train', '123A', 'Crossing international border'),
+            new BoardingCard('Los Angeles', 'Honolulu', 'plane', '123A', ''),
+            new BoardingCard('New York', 'Chicago', 'bus', '123A', ''),
+            new BoardingCard('Honolulu', 'Tokio', 'ship', '123A', 'Crossing international border'),
             new BoardingCard('Tokio', 'Osaka', 'train', '123A', ''),
-            new BoardingCard('Osaka', 'Pyongyang', 'train', '123A', ''),
-            new BoardingCard('Pyongyang', 'Beijing', 'train', '123A', ''),
-            new BoardingCard('Beijing', 'Moscow', 'train', '123A', ''),
-            new BoardingCard('Moscow', 'Minsk', 'train', '123A', ''),
-            new BoardingCard('Minsk', 'Kiev', 'train', '123A', ''),
+            new BoardingCard('Osaka', 'Pyongyang', 'plane', '123A', 'Crossing international border'),
+            new BoardingCard('Pyongyang', 'Beijing', 'train', '123A', 'Crossing international border'),
+            new BoardingCard('Beijing', 'Moscow', 'plane', '123A', 'Crossing international border'),
+            new BoardingCard('Moscow', 'Minsk', 'train', '123A', 'Crossing international border'),
+            new BoardingCard('Minsk', 'Kiev', 'train', '123A', 'Crossing international border'),
         ];
 
         shuffle($cards);
